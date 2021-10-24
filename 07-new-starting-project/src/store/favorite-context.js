1278e64a7bbb50b41df946a9d0d520b53f0e9946
Kavery pm <shiny.kavery@gmail.com>
2021-10-24 11:07:16 -0300

    added state updating logic

--- 07-new-starting-project/src/store/favorite-context.js
+++ 07-new-starting-project/src/store/favorite-context.js
@@ -1,12 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const favoritesContext = createContext({
-favorites:[],
-totalFavorites: 0
+  favorites: [],
+  totalFavorites: 0,
 });
 
-const favoritesContextProvider = (props)=>{
-    const context ={}
-    return <favoritesContext.Provider value={context}>{props.children}</favoritesContext.Provider>
-}
-export default favoritesContextProvider;
+const FavoritesContextProvider = (props) => {
+  const [userFavorites, setuserFavorites] = useState([]);
+  const addFavoriteHandler = (favoriteMeetup) => {
+    setuserFavorites((prevState) => {
+      return prevState.concat(favoriteMeetup);
+    });
+  };
+  const removeFavoritesHandler = (meetupid) => {
+    setuserFavorites((prevState) => {
+      return prevState.filter(meetupid !== prevState.id);
+    });
+  };
+  const isMeetupFavorite = (meetupid)=>{
+      return userFavorites.some(meetup=>meetup.id === meetupid)
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+  };
+  return (
+    <favoritesContext.Provider value={context}>
+      {props.children}
+    </favoritesContext.Provider>
+  );
+};
+export default FavoritesContextProvider;
