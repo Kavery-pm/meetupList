610990b02a2613d95599f93ac44d261b9e6a4690
Kavery pm <shiny.kavery@gmail.com>
2021-10-24 03:59:27 -0300

    added navigation programatically

--- 07-new-starting-project/src/pages/NewMeetup.js
+++ 07-new-starting-project/src/pages/NewMeetup.js
@@ -1,18 +1,27 @@
+import { useHistory } from "react-router";
 import MeetupForm from "../components/meetups/MeetupForm";
 
-const NewMeetups = ()=>{
-    const addnewMeetupHandler = async(meetupData)=>{
-const response = await fetch('https://reacthook-f4714.firebaseio.com/meetups.json',{
-    method: 'POST',
-    body: JSON.stringify(meetupData),
-    headers:{
-        'Content-Type': 'application/json'
-    }
-});
-    }
-return <section>
-    <h1>Add a new Meetup</h1>
-    <MeetupForm onAddNewMeetup={addnewMeetupHandler}></MeetupForm>
-</section>
-}
+const NewMeetups = () => {
+  const history = useHistory();
+
+  const addnewMeetupHandler = async (meetupData) => {
+    await fetch(
+      "https://reacthook-f4714.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    history.replace("/");
+  };
+  return (
+    <section>
+      <h1>Add a new Meetup</h1>
+      <MeetupForm onAddNewMeetup={addnewMeetupHandler}></MeetupForm>
+    </section>
+  );
+};
 export default NewMeetups;
